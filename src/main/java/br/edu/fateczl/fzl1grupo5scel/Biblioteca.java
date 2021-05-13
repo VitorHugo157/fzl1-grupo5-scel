b9908241c4bffba4bc47a38324b6d7000b799af5
Vitor Hugo <f-winchester02@hotmail.com>
2021-05-13 03:08:19 -0300

    Adicionado os casos de teste que funcionam

--- src/main/java/br/edu/fateczl/fzl1grupo5scel/Biblioteca.java
+++ src/main/java/br/edu/fateczl/fzl1grupo5scel/Biblioteca.java
@@ -8,21 +8,31 @@ public class Biblioteca {
     List<Livro> livros = new ArrayList<>();
 
     public void cadastrarLivro(Livro livro) {
+        livros.add(livro);
     }
 
     public boolean isbnJaCadastrado(String isbn) {
+        for(Livro livro : livros) {
+            if(livro.getIsbn().equalsIgnoreCase(isbn)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean isbnEmBranco(String isbn) {
-        return false;
+        return isBlank(isbn);
     }
 
     public boolean tituloEmBranco(String titulo) {
-        return false;
+        return isBlank(titulo);
     }
 
     public boolean autorEmBranco(String autor) {
-        return false;
+        return isBlank(autor);
+    }
+
+    private boolean isBlank(String txt) {
+        return txt == null || txt.trim().isEmpty();
     }
 }
